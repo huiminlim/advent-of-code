4531db10588cfd05516200ff3e9a0014eb9cbe18
huiminlim <e0036379@u.nus.edu>
2022-12-12 23:00:15 +0800

    Add 2022 Day 10 solution

--- 2022/python/day_10/crt.py
+++ 2022/python/day_10/crt.py
@@ -39,3 +39,44 @@ for cmd in commands:
         X += cmd[1]
 
 print(signal)
+
+# Puzzle 2
+# - If sprite is positioned such that one of its
+#   three pixels is the pixel currently being drawn,
+#   the screen produces a lit # pixel, else dark .
+
+
+def draw(cycle, crt, sprite):
+
+    if cycle % 40 == 0 and cycle != 0:
+        crt.append([])
+
+    current_pix_drawing = len(crt[-1])
+    if current_pix_drawing in sprite:
+        crt[-1].append("#")
+    else:
+        crt[-1].append(".")
+
+
+crt = [[]]
+sprite = [0, 1, 2]
+cycle = 0
+for cmd in commands:
+    if "noop" in cmd:
+        draw(cycle, crt, sprite)
+        cycle += 1
+
+    elif "addx" in cmd:
+        left = 2
+        while left > 0:
+            draw(cycle, crt, sprite)
+            left -= 1
+            cycle += 1
+
+        sprite = [x + cmd[1] for x in sprite]
+
+
+for row in crt:
+    for pix in row:
+        print(pix, end="")
+    print()
