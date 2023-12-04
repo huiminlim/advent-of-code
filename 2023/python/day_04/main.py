618333e367957781d9b3e2d75204b1264de0117e
huiminlim <e0036379@u.nus.edu>
2023-12-04 22:48:43 +0800

    add AoC 2023 day 4 part 1

--- 2023/python/day_04/main.py
+++ 2023/python/day_04/main.py
@@ -9,3 +9,19 @@ store = []
 with open(f"{ROOT_DIR}/data.txt", "r") as f:
     for line in f:
         store.append(line.strip())
+
+# Solve Part 1
+score = 0
+for line in store:
+    card = line.split(":")[-1].strip().split("|")
+    winning = [int(x) for x in card[0].strip().split(" ") if x != ""]
+    match = [int(x) for x in card[1].strip().split(" ") if x != ""]
+
+    count = 0
+    for num in winning:
+        if num in match:
+            count += 1
+    if count > 0:
+        score += 2 ** (count - 1)
+
+print(score)
